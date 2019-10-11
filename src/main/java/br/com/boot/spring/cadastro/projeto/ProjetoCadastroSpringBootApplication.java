9bda07ec4affdc84c51e3a5d227968c2323f5a5f
Eduardo Almeida <eduardomatosalmeida@gmail.com>
2019-10-10 23:18:01 -0300

    first commit

---
+++ src/main/java/br/com/boot/spring/cadastro/projeto/ProjetoCadastroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.boot.spring.cadastro.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadastroSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadastroSpringBootApplication.class, args);
+	}
+
+}
